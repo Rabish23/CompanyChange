0e268926ef9f7ff02f4a7ca85b24d56059da12b4
Rabish23 <rabishkumar2323@gmail.com>
2021-04-07 01:26:06 +0530

    added new book by parent company

--- src/practice/AirPlane.java
+++ src/practice/AirPlane.java
@@ -4,6 +4,7 @@ public class AirPlane {
 	
 	public static void main(String args[]) {
 		System.out.println("Rabish");
+		System.out.println("RabishCGI");
 	}
 
 }
--- src/practice/Book.java
+++ src/practice/Book.java
@@ -1,5 +1,9 @@
 package practice;
 
 public class Book {
+	
+	public static void main(String[] args) {
+		System.out.println("Alchemist");
+	}
 
 }
